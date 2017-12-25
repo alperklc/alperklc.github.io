0f136d6e69f1395aa5f05b5fda0c1198bdada47a
alperklc <alperkilci@gmail.com>
2017-12-25 16:27:22 +0100

    update port

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 const app = express();
 
 app.use(serveStatic(__dirname));
