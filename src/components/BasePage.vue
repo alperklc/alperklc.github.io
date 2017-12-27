d0df8510613a7572842415a8d80c6b6342cc227a
alperklc <alperkilci@gmail.com>
2017-12-27 18:02:27 +0100

    move navigation logic to page header

--- src/components/BasePage.vue
+++ src/components/BasePage.vue
@@ -2,18 +2,14 @@
   <div class="page-container">
     <page-header
       :title="title"
-      :number="numberToNavigate"
+      :initialPageNumber="number"
     />
     <slot name="pageContent" />
   </div>
 </template>
 
 <script>
-import debounce from 'debounce';
 import PageHeader from './PageHeader';
-
-const VALID_PAGES = ['100', '200', '400'];
-const NAVIGATION_TIMEOUT = 1000;
 
 export default {
   components: { PageHeader },
@@ -21,58 +17,10 @@ export default {
     title: String,
     number: String,
   },
-  data() {
-    return {
-      numberToNavigate: '',
-      typingPageNumber: false,
-    };
-  },
-  watch: {
-    numberToNavigate: debounce(function () {
-      if (this.numberToNavigate !== this.number) {
-        this.navigatePage(this.numberToNavigate);
-      }
-    }, NAVIGATION_TIMEOUT),
-  },
-  methods: {
-    navigatePage(numberToNavigate) {
-      this.typingPageNumber = false;
-
-      if (this.isPageValid(numberToNavigate)) {
-        this.$router.push({ path: numberToNavigate });
-      } else {
-        this.numberToNavigate = this.number;
-      }
-    },
-    isPageValid(numberToNavigate) {
-      return !!VALID_PAGES.find(page => page === numberToNavigate);
-    },
-    isNumeric(keyCode) {
-      return keyCode >= 48 && keyCode <= 57;
-    },
-    handleKeydown(e) {
-      if (this.isNumeric(e.keyCode)) {
-        if (this.typingPageNumber) {
-          this.numberToNavigate = this.numberToNavigate.concat(e.key);
-        } else {
-          this.numberToNavigate = e.key;
-          this.typingPageNumber = true;
-        }
-      }
-    },
-  },
-  mounted() {
-    this.numberToNavigate = this.number;
-    document.addEventListener('keydown', this.handleKeydown);
-  },
-  beforeDestroy() {
-    document.removeEventListener('keydown', this.handleKeydown);
-  },
 };
 </script>
 
 <style>
-
 .page-container {
   width: 100%;
   margin: 0 auto;
@@ -82,5 +30,4 @@ export default {
   height: 0;
   padding-bottom: 75%;
 }
-
 </style>
