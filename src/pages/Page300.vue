6d938cef2f2838373050ead50481d8044be68a55
alperklc <alperkilci@gmail.com>
2018-01-05 16:31:40 +0100

    rearrange pages

--- src/pages/Page300.vue
+++ src/pages/Page300.vue
@@ -4,19 +4,33 @@
       class="page"
       slot="pageContent">
       <div class="page__banner">
-        <span class="banner-text banner-text--top">( ͡° ͜ʖ ͡°)</span>
-        <span class="banner-text banner-text--bottom">BLOG</span>
+        <span class="banner-text banner-text--top">(⌐■_■)</span>
+        <span class="banner-text banner-text--bottom">CONTACT</span>
       </div>
-      <div class="text">
-        I wrote some posts about travelling, technology and personal things on different blogs in the past.
-        Created my own blog recently, writing sometimes.
-        My current blog is written in my mothertongue, Turkish.
+      <div></div>
+      <div class="contact-links">
+        <span class="contact-link">
+          <span>E-Mail:</span>
+          <a class="link nostyle" href="mailto:alperkilci@gmail.com">alperkilci@gmail.com</a>
+        </span>
+        <span class="contact-link">
+          <span>Twitter:</span>
+          <a class="link nostyle" href="https://www.twitter.com/alperql">twitter.com/alperql</a>
+        </span>
+        <span class="contact-link">
+          <span>Github:</span>
+          <a class="link nostyle" href="https://github.com/alperklc/">github.com/alperklc</a>
+        </span>        
+        <span class="contact-link">
+          <span>LinkedIn:</span>
+          <a class="link nostyle" href="https://www.linkedin.com/in/alper-kılcı-39a55472">linkedin.com/in/alper-kılcı-39a55472</a>
+        </span>
+        <span class="contact-link">
+          <span>Xing:</span>
+          <a class="link nostyle" href="https://www.xing.com/profile/Alper_KILCI">xing.com/profile/Alper_KILCI</a>
+        </span>
       </div>
-      <div class="text">
-        Wouldn't it be nice to have few words automatically from the most recent blog posts here? Well, already added to the backlog!
-      </div>
-      <div class="text">
-        Here it is: <a class="link nostyle" href="https://blog.alperkilci.com">blog.alperkilci.com</a>
+      <div>
       </div>
     </div>
   </base-page>
@@ -29,3 +43,12 @@ export default {
   components: { BasePage },
 };
 </script>
+
+<style>
+.contact-link {
+  display: block;
+  margin: 0 20px;
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+}
+</style>
