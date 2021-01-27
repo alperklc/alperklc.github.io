21c42f76c58c5e22aca3b236475733baa114c503
Alper Kilci <alperkilci@gmail.com>
2021-01-27 16:43:38 +0100

    WIP: experimenting with htm and preact

---
+++ bundle-free/js/date-time-display.js
@@ -0,0 +1,33 @@
+import {
+  html,
+  useState,
+  useEffect,
+} from "https://unpkg.com/htm/preact/standalone.module.js";
+
+const DateTimeDisplay = () => {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const updateCurrentTime = () => {
+    setCurrentTime(new Date());
+  };
+
+  useEffect(() => {
+    setInterval(updateCurrentTime, 1000);
+
+    return () => {
+      clearInterval(updateCurrentTime);
+    };
+  });
+
+  const date = currentTime.toDateString().toUpperCase().slice(0, -5);
+  const time = currentTime.toLocaleTimeString();
+
+  return html`
+    <span class="datetime">
+      <span class="date">${date}</span>
+      <span class="time">${time}</span>
+    </span>
+  `;
+};
+
+export default DateTimeDisplay;
